ca4bf4807251886a3d870fd4ad7a66851f97005a
ozgunsungar <ozgunsungar@gmail.com>
2023-11-09 06:10:43 +0300

    db connections

--- laracast/section2/index.php
+++ laracast/section2/index.php
@@ -2,4 +2,12 @@
 
 require 'functions.php';
 #dd($_SERVER["REQUEST_URI"]);
-require 'router.php';
+#require 'router.php';
+
+//connect to our MySQL database
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+$pdo = new PDO($dsn);
+$statement = $pdo->prepare("select * from posts");
+$statement-> execute();
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+dd($posts);
