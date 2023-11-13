51ca623ee9ae2238092f6dcf114e846b94d65f0b
ozgunsungar <ozgunsungar@gmail.com>
2023-11-13 10:33:48 +0300

    end of the section2

--- section2/index.php
+++ section2/index.php
@@ -4,7 +4,12 @@ require 'functions.php';
 //require 'router.php';
 require 'Database.php';
 
-$db = new Database();
-$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+$config = require('config.php');
+$db = new Database($config['database']);
+
+$id = $_GET['id'];
+$query = "select * from posts where id = :id";
+
+$posts = $db->query($query, [':id' => $id])->fetch();
 
 dd($posts);
